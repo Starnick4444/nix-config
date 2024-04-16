84c6748de8d987986f2972fcbf881cf5d4074ead
Starnick4444 <nemes.bence1@gmail.com>
2024-04-16 18:55:41 +0000

    added home manager module to nixos flakes

--- flake.nix
+++ flake.nix
@@ -64,6 +64,15 @@
         modules = [
           # > Our main nixos configuration file <
           ./nixos/configuration.nix
+
+	  home-manager.nixosModules.home-manager
+	  {
+	    home-manager.useGlobalPkgs = true;
+	    home-manager.useUserPackages = true;
+	    home-manager.users = {
+	      ./home-manager/home.nix;
+	    };
+	  };
         ];
       };
     };
