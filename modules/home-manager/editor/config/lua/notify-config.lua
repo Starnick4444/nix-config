fe6407b26c7c213edede8619244d15594c7ad1a7
Starnick4444 <nemes.bence1@gmail.com>
2024-06-30 23:46:24 +0200

    fixed coloring in notify-config

--- modules/home-manager/editor/config/lua/notify-config.lua
+++ modules/home-manager/editor/config/lua/notify-config.lua
@@ -1,29 +1,27 @@
 require("notify").setup({})
 
 -- TODO fix colors, grep for colors and fix accordingly
--- local colors = require("nord-colors")
---[[
+local colors = require("base16-colorscheme").colors
 local util = require("util")
 util.colorize({
 	-- Error
-	NotifyERRORBorder = { fg = colors.nord11_gui },
-	NotifyERRORIcon = { fg = colors.nord11_gui },
-	NotifyERRORTitle = { fg = colors.nord11_gui },
+	NotifyERRORBorder = { fg = colors.base0B },
+	NotifyERRORIcon = { fg = colors.base0B },
+	NotifyERRORTitle = { fg = colors.base0B },
 	-- Warn
-	NotifyWARNBorder = { fg = colors.nord13_gui },
-	NotifyWARNIcon = { fg = colors.nord13_gui },
-	NotifyWARNTitle = { fg = colors.nord13_gui },
+	NotifyWARNBorder = { fg = colors.baseD },
+	NotifyWARNIcon = { fg = colors.baseD },
+	NotifyWARNTitle = { fg = colors.baseD },
 	-- Info
-	NotifyINFOBorder = { fg = colors.nord14_gui },
-	NotifyINFOIcon = { fg = colors.nord14_gui },
-	NotifyINFOTitle = { fg = colors.nord14_gui },
+	NotifyINFOBorder = { fg = colors.base0E },
+	NotifyINFOIcon = { fg = colors.base0E },
+	NotifyINFOTitle = { fg = colors.base0E },
 	-- Debug
-	NotifyDEBUGBorder = { fg = colors.nord10_gui },
-	NotifyDEBUGIcon = { fg = colors.nord10_gui },
-	NotifyDEBUGTitle = { fg = colors.nord10_gui },
+	NotifyDEBUGBorder = { fg = colors.base0A },
+	NotifyDEBUGIcon = { fg = colors.base0A },
+	NotifyDEBUGTitle = { fg = colors.base0A },
 	-- Trace
-	NotifyTRACEBorder = { fg = colors.nord15_gui },
-	NotifyTRACEIcon = { fg = colors.nord15_gui },
-	NotifyTRACETitle = { fg = colors.nord15_gui },
+	NotifyTRACEBorder = { fg = colors.base0F },
+	NotifyTRACEIcon = { fg = colors.base0F },
+	NotifyTRACETitle = { fg = colors.base0F },
 })
---]]
