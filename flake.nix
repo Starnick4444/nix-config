9b0d8b939edc009805b2118cb5c2a8a1820b685a
Starnick4444 <nemes.bence1@gmail.com>
2024-07-03 09:01:46 +0200

    add rust overlay

--- flake.nix
+++ flake.nix
@@ -7,6 +7,8 @@
     # hyprland.url = "git+https://github.com/hyprwm/Hyprland?submodules=1";
     hyprland.url = "github:hyprwm/Hyprland";
 
+    rust-overlay.url = "github:oxalica/rust-overlay";
+
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -24,6 +26,8 @@
       modules = [
         ./nixos/configuration.nix
         inputs.home-manager.nixosModules.default
+	nixpkgs.overlays = [ rust-overlay.overlays.default ];
+        environment.systemPackages = [ pkgs.rust-bin.stable.latest.default ];
       ];
     };
   };
