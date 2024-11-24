f05ea142067280986c3e6c3c983e856e9caa294e
Starnick4444 <nemes.bence1@gmail.com>
2024-11-24 01:07:04 +0100

    workaround for nvim bug, flake update

--- home/nvim/default.nix
+++ home/nvim/default.nix
@@ -26,6 +26,18 @@ _: {
     vimAlias = true;
 
     luaLoader.enable = true;
+
+    # https://github.com/neovim/neovim/issues/30985
+    extraConfigLua = "
+for _, method in ipairs({ 'textDocument/diagnostic', 'workspace/diagnostic' }) do
+    local default_diagnostic_handler = vim.lsp.handlers[method]
+    vim.lsp.handlers[method] = function(err, result, context, config)
+        if err ~= nil and err.code == -32802 then
+            return
+        end
+        return default_diagnostic_handler(err, result, context, config)
+    end
+end";
   };
   # environment.variables.EDITOR = "nvim";
 }
