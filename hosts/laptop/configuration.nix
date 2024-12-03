60834d9f711167ae51edeb9cab129c0c4ed99938
Starnick4444 <nemes.bence1@gmail.com>
2024-12-03 16:18:51 +0100

    re-enable cachyos kernel for laptop

--- hosts/laptop/configuration.nix
+++ hosts/laptop/configuration.nix
@@ -27,8 +27,8 @@ in {
 
     kernel.sysctl."vm.max_heap_count" = 1048576;
     # use latest kernel
-    kernelPackages = pkgs.linuxPackages_latest;
-    # kernelPackages = pkgs.linuxPackages_cachyos;
+    # kernelPackages = pkgs.linuxPackages_latest;
+    kernelPackages = pkgs.linuxPackages_cachyos;
 
     # tmp.useTmpfs = true;
   };
