4a9cd9af4bd64e9034576bf94d9a0f3663eb92b4
Starnick4444 <nemes.bence1@gmail.com>
2024-12-08 13:00:12 +0100

    add nix-ld (disabled rn)

--- linux/games/default.nix
+++ linux/games/default.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }: {
+{pkgs, ...}: {
   programs = {
     steam = {
       enable = true;
@@ -11,6 +11,42 @@
     mangohud
     protonup
   ];
+
+  /*
+  programs.nix-ld = {
+    enable = true;
+
+    libraries = pkgs.steam-run.fhsenv.args.multiPkgs pkgs;
+  };
+  */
+  /*
+  libraries = with pkgs; [
+    xorg.libXau
+    xorg.libXrandr
+    xorg.libX11
+    libGL
+    krb5
+    glib
+    glibc # maybe?
+    libgcc.lib #maybe?
+    nss # or nss3_latest?
+    libz # new
+    nspr
+    xorg.libXcomposite
+    xorg.libXdamage
+    xorg.libXfixes
+    xorg.libXtst
+    freetype
+    expat
+    fontconfig.lib
+    dbus.lib
+    alsa-lib
+    xorg.libXrender
+    xorg.libXdmcp
+
+    libsForQt5.qt5.qtbase
+  ];
+  */
 
   environment.sessionVariables = {
     STEAM_EXTRA_COMPAT_TOOLS_PATH = "/home/starnick/.steam/root/compatibilitytools.d";
