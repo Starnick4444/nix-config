c330f1fc6f88c9d2b31664e7dea316771efca42e
Starnick4444 <nemes.bence1@gmail.com>
2025-03-20 10:17:40 +0100

    nvidia specific config for mainpc

--- hosts/nixos/mainpc/default.nix
+++ hosts/nixos/mainpc/default.nix
@@ -8,8 +8,10 @@
   inputs,
   lib,
   pkgs,
+  config,
   ...
-}: {
+}:
+{
   imports = lib.flatten [
     #
     # ========== Hardware ==========
@@ -67,7 +69,7 @@
         tcp = {
           ssh = 22;
         };
-        upd = {};
+        upd = { };
       };
     };
     hdr = lib.mkForce false;
@@ -103,6 +105,13 @@
   services.geoclue2.geoProviderUrl = "https://api.beacondb.net/v1/geolocate";
 
   boot = {
+    # nvidia gpu sleep, nvidia param for wayland
+    kernelParams = [
+      "nvidia.NVreg_PreserveVideoMemoryAllocations=1"
+      "nvidia-drm.fbdev=1"
+      "nvidia-drm.modeset=1"
+    ];
+
     kernelPackages = pkgs.linuxPackages_latest;
 
     loader = {
@@ -115,10 +124,51 @@
       timeout = 3;
     };
 
+    supportedFilesystems = [ "ntfs" ];
+
     initrd = {
       systemd.enable = true;
     };
+
+    tmp.useTmpfs = true;
   };
+
+  hardware.graphics = {
+    enable = true;
+    enable32Bit = true;
+    extraPackages = with pkgs; [ nvidia-vaapi-driver ];
+  };
+
+  # Load nvidia driver for Xorg and Wayland
+  services.xserver.videoDrivers = [ "nvidia" ];
+
+  hardware.nvidia = {
+    # Modesetting is required.
+    # modesetting.enable = true;
+
+    # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
+    # Enable this if you have graphical corruption issues or application crashes after waking
+    # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead
+    # of just the bare essentials.
+    powerManagement.enable = true;
+
+    # Fine-grained power management. Turns off GPU when not in use.
+    # Experimental and only works on modern Nvidia GPUs (Turing or newer).
+    powerManagement.finegrained = false;
+
+    # Use the NVidia open source kernel module (not to be confused with the
+    # independent third-party "nouveau" open source driver).
+    open = false;
+
+    # Enable the Nvidia settings menu,
+    # accessible via `nvidia-settings`.
+    nvidiaSettings = true;
+
+    # Optionally, you may need to select the appropriate driver version for your specific GPU.
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
+  };
+
+  zramSwap.enable = true;
 
   #hyprland border override example
   #  wayland.windowManager.hyprland.settings.general."col.active_border" = lib.mkForce "rgb(${config.stylix.base16Scheme.base0E});
