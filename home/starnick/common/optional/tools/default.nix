d3e1c843b169e0f90bb734677e732831bf481a09
Starnick4444 <nemes.bence1@gmail.com>
2025-04-11 08:48:54 +0200

    use unstable jujutsu

--- home/starnick/common/optional/tools/default.nix
+++ home/starnick/common/optional/tools/default.nix
@@ -2,32 +2,33 @@
 {
   #imports = [ ./foo.nix ];
 
-  home.packages = builtins.attrValues {
-    inherit (pkgs)
-      # Development
-      tokei
-      jujutsu
-      # Device imaging
-      # rpi-imager
-      #etcher #was disabled in nixpkgs due to dependency on insecure version of Electron
-      # Productivity
-      drawio
-      grimblast
-      libreoffice
-      # Privacy
-      #veracrypt
-      #keepassxc
-      # Media production
-      # blender-hip # -hip variant includes h/w accelrated rendering with AMD RNDA gpus
-      # gimp
-      # inkscape
-      obs-studio
-      # VM and RDP
-      # remmina
-      fastfetch
-      onefetch
-      ;
-  };
+  home.packages =
+    builtins.attrValues {
+      inherit (pkgs)
+        # Development
+        tokei
+        # Device imaging
+        # rpi-imager
+        #etcher #was disabled in nixpkgs due to dependency on insecure version of Electron
+        # Productivity
+        drawio
+        grimblast
+        libreoffice
+        # Privacy
+        #veracrypt
+        #keepassxc
+        # Media production
+        # blender-hip # -hip variant includes h/w accelrated rendering with AMD RNDA gpus
+        # gimp
+        # inkscape
+        obs-studio
+        # VM and RDP
+        # remmina
+        fastfetch
+        onefetch
+        ;
+    }
+    ++ [ pkgs.unstable.jujutsu ];
   #Disabled for now. grimblast
   #  services.flameshot = {
   #      enable = true;
