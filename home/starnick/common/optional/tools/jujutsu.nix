b3ab6e3470a90635979629095b4c947c5458fadc
Starnick4444 <nemes.bence1@gmail.com>
2025-08-24 13:22:37 +0200

    allow extra args for jj cl-up

--- home/starnick/common/optional/tools/jujutsu.nix
+++ home/starnick/common/optional/tools/jujutsu.nix
@@ -14,10 +14,21 @@
           ''
             set -euo pipefail
             INPUT=''${1:-"@-"}
+            shift  # Remove the first argument, leaving potential extra arguments in $@
             HASH=$(jj log -r "''${INPUT}" -T commit_id --no-graph)
             HASHINFO=$(git log -n 1 ''${HASH} --oneline --color=always)
+
+            # Start with the base parameter
+            PARAMS="l=Autosubmit+1"
+
+            # Append additional arguments with commas (if any exist)
+            if [ $# -gt 0 ]; then
+              EXTRA_ARGS=$(printf "%s" "$@" | paste -sd "," -)
+              PARAMS="''${PARAMS},''${EXTRA_ARGS}"
+            fi
+
             echo "Pushing from commit ''${HASHINFO}"
-            git push origin "''${HASH}":refs/for/canon
+            git push origin "''${HASH}":refs/for/canon%''${PARAMS}
           ''
           ""
         ];
